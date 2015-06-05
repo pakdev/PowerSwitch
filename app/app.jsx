9fee892580a4bec112a1b1b49202ecfca47fe7bc
Peter Kurlak <pkurlak@gmail.com>
2015-06-05 17:44:03 -0500

    removing artifacts

--- app/app.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var Main = require('./scripts/main.jsx');
-
-React.render(
-    <Main />,
-    document.getElementById('main')
-);
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,95 @@
 var gulp = require('gulp');
 
+var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
 var connect = require('gulp-connect');
-var reactify = require('reactify');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('autoprefixer-core');
+
+var through = require('through2');
+var coffeeify = require('coffeeify');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
+
 
 var paths = {
-    scripts: 'app/scripts/*.jsx',
-    vendorStyles: [
+    ASSETS: [
+        'app/assets/**/*'
+    ],
+    SCRIPT: 'app/scripts/app.coffee',
+    SCRIPTS: [
+        'app/scripts/**/*.coffee',
+    ],
+    STYLES: [
+        'app/styles/*.scss'
+    ],
+    VENDOR_SCRIPTS: [
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-resource/angular-resource.js',
+        'bower_components/angular-pretty-checkable/dist/angular-pretty-checkable.min.js',
+        'bower_components/angular-ui-bootstrap-bower/ui-bootstrap.js',
+        'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js'
+    ],
+    VENDOR_STYLES: [
         'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bower_components/angular-pretty-checkable/dist/pretty-checkable.css'
     ]
 };
 
-gulp.task('js', function() {
-    browserify('./app/app.jsx')
-        .transform('reactify', { es6: true })
-        .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('public/js'))
-    .pipe(connect.reload());
+gulp.task('assets', function() {
+    gulp.src(paths.ASSETS, {base: 'app/assets'})
+        .pipe(gulp.dest('public'))
+        .pipe(livereload());
+        //.pipe(connect.reload());
 });
 
-gulp.task('css', function() {
-    return gulp.src(paths.vendorStyles)
-    .pipe(concat('vendor.css'))
-    .pipe(gulp.dest('public/css'))
-    .pipe(connect.reload());
+gulp.task('scripts', function() {
+    var browserified = function() {
+        return through.obj(function(chunk, enc, callback) {
+            if (chunk.isBuffer()) {
+                var b = browserify(chunk.path).transform(coffeeify);
+                chunk.contents = b.bundle();
+                this.push(chunk);
+            }
+
+            callback();
+        });
+    };
+
+    gulp.src(paths.SCRIPT)
+        .pipe(browserified())
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public/js')),
+    gulp.src(paths.VENDOR_SCRIPTS)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(livereload());
+        //.pipe(connect.reload());
+});
+
+gulp.task('styles', function() {
+    gulp.src(paths.STYLES)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('app.css'))
+        .pipe(postcss([autoprefixer({ browsers: ['last 1 version' ] })]))
+        .pipe(gulp.dest('public/css')),
+    gulp.src(paths.VENDOR_STYLES)
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(livereload());
+        //.pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-    gulp.watch([paths.scripts], ['js']);
+    livereload.listen();
+    gulp.watch(paths.ASSETS, ['assets']);
+    gulp.watch(paths.SCRIPTS, ['scripts']);
+    gulp.watch(paths.STYLES, ['styles']);
 });
 
 gulp.task('connect', function() {
@@ -41,5 +99,6 @@ gulp.task('connect', function() {
     });
 });
 
-gulp.task('default', ['js', 'css', 'connect', 'watch']);
+gulp.task('default', ['assets', 'scripts', 'styles', 'watch']);
+gulp.task('serve', ['assets', 'scripts', 'styles', 'watch', 'connect']);
 
